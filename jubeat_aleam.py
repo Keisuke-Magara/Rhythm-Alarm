c0ca22b942c20625fca67cde5e6abc69814c1c9a
Keisuke Magara <Keisuke-Magara@users.noreply.github.com>
2021-06-30 10:48:21 +0900

    backup

--- jubeat_aleam.py
+++ jubeat_aleam.py
@@ -9,7 +9,7 @@ from PIL import Image, ImageTk
 import sys
 import time
 #自作モジュールインポート
-#import Music
+#from music import playsound
 #import Score
 #import Alarm
 #import Panel
@@ -20,15 +20,25 @@ import time
 
 class Panel(ttk.Button):
     bright:bool = False
-    img = 0
+    imgName = "./buttonImageTest.png"
+
+    def __init__ (master=None, text=None, image=None, name=None, padding=None):
+        super.__init__(master, text, name, padding)
+
+
+
+    def callfor(self): # ボタンが押されたときに実行される処理 (panelが押されたパネルを示す)
+        def inner(): # 実際に呼び出されるのはこっち (この中に処理を記述)
+            panel.configure(text="pushd.")
+        return inner
 
     #def refresh(): #ボタンのプロパティを反映する関数
 
-#ウィンドウ描画
-class JubeatFrame (ttk.Frame):
+
+class JubeatFrame (ttk.Frame): # ゲーム画面描画
     panel = []
-    img = Image.open("./buttonImageTest.png")
-    img = ImageTk.PhotoImage(img)
+    #img = Image.open("./buttonImageTest.png")
+    #img = ImageTk.PhotoImage(file="./buttonImageTest.png")
 
     def __init__(self, master):
         super().__init__(master)
@@ -39,29 +49,37 @@ class JubeatFrame (ttk.Frame):
 
     def create_widgets (self):
         #display 9 panlels.
-        #img = Image.open("./buttonImageTest.png")
-        #img = ImageTk.PhotoImage(img)
+        img = Image.open(self.imgName)
+        img = ImageTk.PhotoImage(img)
         #img = tk.PhotoImage(file="./Test.png")
         for i in range (9):
             width = 35 #px
             height = 56 #px
-            #padding=(width, height, width, height), 
-            self.panel.append(Panel(master=self, image=self.img, command=self.callfor(), name="Panel_"+str(i)))
-        y = 255
-        self.panel[0].place(x=0, y=0.1 + y)
-        self.panel[1].place(x=145, y=0.1 + y)
-        self.panel[2].place(x=290, y=0.1 + y)
-        self.panel[3].place(x=0, y=137 + y)
-        self.panel[4].place(x=145, y=137 + y)
-        self.panel[5].place(x=290, y=137 + y)
-        self.panel[6].place(x=0, y=274 + y)
-        self.panel[7].place(x=145, y=274 + y)
-        self.panel[8].place(x=290, y=274 + y)
-    
-    def callfor(self):
-        def inner (): # 実際に呼び出されるのはこっち
-            print ("Push Detected.\n")
-        return inner
+            #, padding=(width, height, width, height)
+            self.panel.append(Panel(self, image=img, name="panel_"+str(i), padding=(width, height, width, height)))
+            pushd = self.callfor(self.panel[i])
+            #self.panel[i].config(command=pushd)
+        blank = 255
+        self.panel[0].place(x=0, y=0.1 + blank)
+        self.panel[1].place(x=145, y=0.1 + blank)
+        self.panel[2].place(x=290, y=0.1 + blank)
+        self.panel[3].place(x=0, y=137 + blank)
+        self.panel[4].place(x=145, y=137 + blank)
+        self.panel[5].place(x=290, y=137 + blank)
+        self.panel[6].place(x=0, y=274 + blank)
+        self.panel[7].place(x=145, y=274 + blank)
+        self.panel[8].place(x=290, y=274 + blank)
+
+
+
+
+class GameStart:
+    timing = [[0]*10]
+    pre_sec = 2 # call collisionDetection() pre_sec second before touch time.
+
+    def __init__ ():
+        music.playsound()
+
 
 def main():
     #rootメインウィンドウの設定
@@ -80,6 +98,7 @@ def main():
     #app = Application(master=root)
     #frame_jubeatを最前面にする
     frame_jubeat.tkraise()
+    #game = GameStart
     root.mainloop()
 
 if __name__ == "__main__":
