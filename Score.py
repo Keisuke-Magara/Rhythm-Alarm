3deed367ea8657908aeb754ccc71a6909573fa4e
chihatank <86899179+chihatank@users.noreply.github.com>
2021-07-07 11:24:45 +0900

    Add files via upload

--- Score.py
+++ Score.py
@@ -21,18 +21,10 @@ class Score:
         self.tile7 = [x[0] for x in music_list if x[7]==1]
         self.tile8 = [x[0] for x in music_list if x[8]==1]
         self.tile9 = [x[0] for x in music_list if x[9]==1]
-        
-    #時間計測
-    def starting(self):
-        self.start = time.perf_counter()
-        while True:
-            #elatime=経過時間(単位はms)
-            self.elatime = (time.perf_counter() - self.start) * 1000
-            time.sleep(0.01) #約0.01秒ごとに更新
 
          
-    #引数:タイルの番号i 指定したタイルの判定範囲に応じたスコアを返す
-    def determineScore(self,i):
+    #引数:タイルの番号i,経過時間elatime 指定したタイルの判定範囲に応じたスコアを返す
+    def determineScore(self,i,elatime):
         #タイルの指定
         if i == 1:
             self.tile = self.tile1
@@ -52,13 +44,13 @@ class Score:
             self.tile = self.tile9
 
         #判定(perfect=100点, great=70点, good= 50点, bad=10点)
-        if any(self.elatime-self.perfect < t < self.elatime+self.perfect for t in self.tile):
+        if any(elatime-self.perfect < t < elatime+self.perfect for t in self.tile):
             self.score = 100 #perfect
-        elif any(self.elatime-self.great < t < self.elatime+self.great for t in self.tile):
+        elif any(elatime-self.great < t < elatime+self.great for t in self.tile):
             self.score = 70 #great
-        elif any(self.elatime-self.good < t < self.elatime+self.good for t in self.tile):
+        elif any(elatime-self.good < t < elatime+self.good for t in self.tile):
             self.score = 50 #good
-        elif any(self.elatime-self.bad < t < self.elatime+self.bad for t in self.tile):
+        elif any(elatime-self.bad < t < elatime+self.bad for t in self.tile):
             self.score = 10 #bad
         else:
             self.score = 0 #miss
