935ba883163f5c47f5136ff70525512022a521b0
Keisuke Magara <61485115+Keisuke-Magara@users.noreply.github.com>
2021-07-13 01:11:19 +0900

    Prepare for Pre-release.

---
+++ Rythm Alarm/JubeatFrame.py
@@ -0,0 +1,65 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter.constants import Y
+from PIL import Image, ImageTk
+import Panel
+import datetime
+
+class JubeatFrame (ttk.Frame): # ゲーム画面描画
+    def __init__(self, master, score, parent, time):
+        #self.configure(bg="black")
+        self.goal_score = 15000 # この点数に達するまで永遠にループ
+        self.total_score = 0
+        self.root = master
+        self.parent = parent
+        self.score_class = score # Scoreクラスのインスタンスの参照
+        self.timer = time # MusicTimerクラスのインスタンスの参照
+        self.panel = []
+        self.style = ttk.Style()
+        self.style.theme_use("alt")
+        self.style.configure("GameStyle.TFrame", background="black")
+        super().__init__(master, style="GameStyle.TFrame")
+        self.msg = tk.StringVar()
+        self.msg.set("こいつを止めるには、\nクリアするしかない。")
+        self.clock = tk.StringVar()
+        self.music_name = tk.StringVar()
+        self.music_name.set("アラーム音: 夜に駆ける")
+        self.score = tk.StringVar()
+        self.create_widgets()
+        self.root.after(1, self.repeat_processes)
+
+    def create_widgets (self):
+        # create clock area.
+        self.clock_area = tk.Label(self, bg="brown", width=15, height = 1, font=("MSゴシック", 25, "bold"), textvariable=self.clock)
+        self.clock_area.place(x=70, y=0)
+        # create message area.
+        self.message_area = tk.Label(self, bg="gold", width=26, height = 2, font=("MSゴシック", 20, "bold"), textvariable=self.msg)
+        self.message_area.place(x=0, y=50)
+        # create music name area.
+        self.music_name_area = tk.Label(self, bg="black", fg="white", width=24, height=1, font=("MSゴシック", 25, "normal"), textvariable=self.music_name)
+        self.music_name_area.place(x=0, y=130)
+        # create Score area.
+        self.score_area = tk.Label(self, bg="green", width=10, height = 1, font=("MSゴシック", 40, "bold"), textvariable=self.score)
+        self.score_area.place(x=120, y=180)
+        # create 9 panlels.
+        for i in range (9):
+            width = 10 # "0" size.
+            height = 56 #px
+            self.panel.append(Panel.Panel(master=self, score=self.score_class, time=self.timer, name=str(i), text=str(i)))
+        xoffset = 5
+        yoffset = 255
+        self.panel[0].place(x=0+xoffset, y=0+yoffset)
+        self.panel[1].place(x=145+xoffset, y=0+yoffset)
+        self.panel[2].place(x=290+xoffset, y=0+yoffset)
+        self.panel[3].place(x=0+xoffset, y=140+yoffset)
+        self.panel[4].place(x=145+xoffset, y=140+yoffset)
+        self.panel[5].place(x=290+xoffset, y=140+yoffset)
+        self.panel[6].place(x=0+xoffset, y=278+yoffset)
+        self.panel[7].place(x=145+xoffset, y=278+yoffset)
+        self.panel[8].place(x=290+xoffset, y=278+yoffset)
+    
+    def repeat_processes(self):
+        now = datetime.datetime.now()
+        self.clock.set(now.strftime('%H:%M:%S'))
+        self.score.set(str(self.total_score) + " / " + str(self.goal_score))
+        self.root.after(100, self.repeat_processes)
---
+++ Rythm Alarm/Panel.py
@@ -0,0 +1,105 @@
+from os import name, times
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+import sound
+class Panel(ttk.Button):
+    bright:bool = False
+    defaultImg_name = "./default.png"
+    brightImg_name = "./Box.gif" # 128*128px
+    perfectImg_name = "./perfect.png"
+    greatImg_name = "./great.png"
+    goodImg_name = "./good.png"
+    badImg_name = "./bad.png"
+    fps = 14 # GIF画像は28fpsです。pyinstallerでexeファイル化するときは
+
+    def __init__ (self, master, score, time, text=None, imageName=None, name=None, padding=None, width=None):
+        self.parent = master
+        self.score = score
+        self.timer = time
+        self.style = ttk.Style()
+        self.style.configure("GameStyle.TButton", background="blue")
+        super().__init__(master=master, name=name, width=width)
+        self.defaultimg = None
+        self.bright_img = None
+        self.moveable = False
+        self.root = master
+        self.gif_index = 0
+        self.configure(command=lambda:self.callfor())
+        if (text != None):
+            self.configure(text=text)
+        if (imageName != None):
+            self.defaultImg_name = imageName
+        #self.default_img = Image.open(self.defaultImg_name)
+        #self.default_img = ImageTk.PhotoImage(self.default_img)
+        self.default_img = tk.PhotoImage(file=self.defaultImg_name)
+        self.configure(image=self.default_img)
+        self.bright_img = tk.PhotoImage(file=self.brightImg_name)
+        self.perfect_img = tk.PhotoImage(file=self.perfectImg_name)
+        self.great_img = tk.PhotoImage(file=self.greatImg_name)
+        self.good_img = tk.PhotoImage(file=self.goodImg_name)
+        self.bad_img = tk.PhotoImage(file=self.badImg_name)
+        if (padding != None):
+            self.configure(padding=padding)
+        #self.bright()
+    
+
+    def bright(self):
+        self.moveable = True
+        self.next_frame()
+    
+    def next_frame(self):
+        try: #(2)show_wellの表示が上書きされないようにself.moveable == trueの条件を加えた
+            if self.moveable == True:
+                self.configure(image=self.bright_img)
+                self.bright_img.configure(format="gif -index {}".format(self.gif_index))
+                self.gif_index += 1
+        except tk.TclError:
+            if self.moveable == True:
+                self.set_default()
+        else:
+            if self.moveable == True:
+                self.root.after(int(1000/self.fps), self.next_frame)
+
+    
+
+    def callfor(self): # ボタンが押されたときに実行される処理 (panelが押されたパネルを示す)
+        self.moveable = False
+        sound.play_se('SE_panel.mp3') #(3)ボタンを押したときにSEを流す
+        cur_score = self.score.determineScore(int(self.cget("text"))+1, int(self.timer.get_time()))
+        self.show_well(cur_score)
+        #self.root.after(int(1000/self.fps), self.show_well(cur_score))
+        self.parent.total_score += cur_score
+        #print(cur_score)
+        if self.parent.total_score >= self.parent.goal_score:
+            self.parent.parent.stop_game() #(4)終了処理を追加
+    
+    def show_well (self, score):
+        if (score == 100):
+            self.configure(image=self.perfect_img)
+        elif (score == 70):
+            self.configure(image=self.great_img)
+        elif (score == 50):
+            self.configure(image=self.good_img)
+        elif (score == 10):
+            self.configure(image=self.bad_img)
+        self.root.after(250, self.set_default)
+
+    def set_default(self):
+        self.configure(image=self.default_img)
+        self.gif_index = 0
+
+    #def refresh(): #ボタンのプロパティを反映する関数
+
+    #def bright(self)
+    #def debright()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    frame = tk.Frame(root)
+    frame.pack(fill = tk.BOTH, padx=20,pady=10)
+    panel = Panel(root)
+    panel.pack()
+    #panel.bright()
+    root.mainloop()
